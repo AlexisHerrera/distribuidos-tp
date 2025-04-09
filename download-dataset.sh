6ef6da1bbc6b627c7f874b1f3c6bbbadbd0ae93f
Alexis Herrera <alexysaguilar@gmail.com>
2025-04-09 20:04:42 -0300

    Unziping datasets at .data

--- download-dataset.sh
+++ download-dataset.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-curl -L -o ./the-movies-dataset.zip https://www.kaggle.com/api/v1/datasets/download/rounakbanik/the-movies-dataset
+data_dir="./.data"
+zip_file="the-movies-dataset.zip"
+zip_path="${data_dir}/${zip_file}"
+
+mkdir -p "${data_dir}"
+curl -L -o "${zip_path}" https://www.kaggle.com/api/v1/datasets/download/rounakbanik/the-movies-dataset
+if [ $? -eq 0 ]; then
+  unzip -o "${zip_path}" -d "${data_dir}"
+  if [ $? -eq 0 ]; then
+    echo "Unziped datasets at '${data_dir}'."
+  else
+    echo "Error unziping file."
+  fi
+else
+  echo "Error downloading file."
+fi
+
+exit 0
