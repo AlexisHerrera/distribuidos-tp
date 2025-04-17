9264baa285619e4b04ea4f22d9f7261bf2b4e2fc
fedepenic <fedepenic@hotmail.com>
2025-04-17 23:03:49 +0000

    feat: parse movies file.

--- src/server/cleaner/main.py
+++ src/server/cleaner/main.py
@@ -1,9 +1,54 @@
 import logging
 import os
 from common.socket_communication import receive_message, accept_new_connection, create_server_socket
+from model.movie import Movie
+from io import StringIO
+import csv
+import ast
 
 
 logging.basicConfig(level=logging.INFO)
+
+def dictionary_to_list(dictionary_str):
+    try:
+        dictionary_list = ast.literal_eval(dictionary_str)
+        return [data['name'] for data in dictionary_list]
+    except (ValueError, SyntaxError):
+        return []
+
+def parse_movie(line: str) -> Movie:
+    try:
+        csv_reader = csv.reader(StringIO(line), skipinitialspace=True)
+        fields = next(csv_reader)
+
+        if len(fields) < 24:
+            raise ValueError("Line does not contain enough fields to parse.")
+
+        movie_id = int(fields[5])
+        title = fields[20].strip()
+        genres = dictionary_to_list(fields[3].strip())
+        production_countries = dictionary_to_list(fields[13].strip())
+        release_date = fields[14].strip()
+        budget = int(fields[2])
+        revenue = int(fields[15])
+        overview = fields[10].strip()
+
+        return Movie(
+            movie_id=movie_id,
+            title=title,
+            genres=genres,
+            release_date=release_date,
+            production_countries=production_countries,
+            budget=budget,
+            revenue=revenue,
+            overview=overview
+        )
+
+    except Exception as e:
+        logging.warning(f"Failed to parse line: {line}. Error: {e}")
+        return None
+
+
 
 def receive_movies(client_socket):
     while True:
@@ -12,7 +57,13 @@ def receive_movies(client_socket):
             logging.info("EOF_MOVIES message received.")
             break
         else:
-            logging.info(f'Movie Batch: {message}')
+            logging.info(f'Movie Batch Received:\n{message}')
+            movies = message.strip().split('\n')
+            for movie in movies:
+                parsed_movie = parse_movie(movie)
+                if parsed_movie:
+                    logging.info(parsed_movie)
+    
     client_socket.close()
 
 def main():
