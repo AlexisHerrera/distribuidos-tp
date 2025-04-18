4e40d4e040923b27ba79abaae1cd7e92b7f887e7
SBen-IV <sinneo@fi.uba.ar>
2025-04-18 10:28:09 -0300

    feat: add movie_budget_counter protocol; refactored message

--- src/messaging/protocol/message.py
+++ src/messaging/protocol/message.py
@@ -2,7 +2,9 @@ from enum import Enum
 
 from src.messaging.protocol.cast import CastProtocol
 from src.messaging.protocol.eof import EOFProtocol
+from src.messaging.protocol.message_protocol import MessageProtocol
 from src.messaging.protocol.movie import MovieProtocol
+from src.messaging.protocol.movie_budget_counter import MovieBudgetCounterProtocol
 from src.messaging.protocol.movie_sentiment import MovieSentimentProtocol
 from src.messaging.protocol.null import NullProtocol
 from src.messaging.protocol.rating import RatingProtocol
@@ -15,7 +17,7 @@ class MessageType(Enum):
     Cast = 3
     MovieSentiment = 4
     MovieAvgBudget = 5
-    MovieBudgetCount = 6
+    MovieBudgetCounter = 6
     EOF = 100
 
     @classmethod
@@ -32,19 +34,7 @@ class Message():
         self.data = data
 
     def to_bytes(self) -> bytes:
-        encoder = NullProtocol() # Default encoder
-
-        match self.message_type:
-            case MessageType.Movie:
-                encoder = MovieProtocol()
-            case MessageType.Rating:
-                encoder = RatingProtocol()
-            case MessageType.Cast:
-                encoder = CastProtocol()
-            case MessageType.MovieSentiment:
-                encoder = MovieSentimentProtocol()
-            case MessageType.EOF:
-                encoder = EOFProtocol()
+        encoder = Message.__get_protocol(self.message_type)
 
         (data_encoded, bytes_amount) = encoder.to_bytes(self.data)
         msg_type_encoded = Message.__int_to_bytes(self.message_type.value, Message.MSG_TYPE_LEN)
@@ -57,18 +47,8 @@ class Message():
         msg_type_from_buf = Message.__int_from_bytes(buf, Message.MSG_TYPE_LEN)
         bytes_amount = Message.__int_from_bytes(buf[Message.MSG_TYPE_LEN:], Message.MSG_LEN_SIZE)
 
-        decoder = NullProtocol() # Default decoder
         msg_type = MessageType(msg_type_from_buf)
-
-        match msg_type:
-            case MessageType.Movie:
-                decoder = MovieProtocol()
-            case MessageType.Rating:
-                decoder = RatingProtocol()
-            case MessageType.Cast:
-                decoder = CastProtocol()
-            case MessageType.EOF:
-                decoder = EOFProtocol()
+        decoder = Message.__get_protocol(msg_type)
 
         data: list | None = decoder.from_bytes(buf[Message.MSG_TYPE_LEN + Message.MSG_LEN_SIZE:], bytes_amount)
 
@@ -81,3 +61,21 @@ class Message():
     @staticmethod
     def __int_to_bytes(value: int, bytes_amount: int) -> bytes:
         return value.to_bytes(bytes_amount, 'big', signed=False)
+
+    @staticmethod
+    def __get_protocol(msg_type: MessageType) -> MessageProtocol: # pylint: disable=too-many-return-statements
+        match msg_type:
+            case MessageType.Movie:
+                return MovieProtocol()
+            case MessageType.Rating:
+                return RatingProtocol()
+            case MessageType.Cast:
+                return CastProtocol()
+            case MessageType.MovieSentiment:
+                return MovieSentimentProtocol()
+            case MessageType.MovieBudgetCounter:
+                return MovieBudgetCounterProtocol()
+            case MessageType.EOF:
+                return EOFProtocol()
+            case _:
+                return NullProtocol()
---
+++ src/messaging/protocol/movie_budget_counter.py
@@ -0,0 +1,32 @@
+# pylint: disable=no-member
+from src.messaging.protobuf import movie_budget_counters_pb2
+from src.messaging.protocol.message_protocol import MessageProtocol
+from src.model.movie_budget_counter import MovieBudgetCounter
+
+
+class MovieBudgetCounterProtocol(MessageProtocol):
+    def __init__(self):
+        super().__init__(item_to_bytes=self.__to_movie_budget_counter_pb,
+                        encode_all=self.__encode_all,
+                        bytes_to_item=self.__to_movie_budget_counter,
+                        decode_all=self.__decode_all)
+
+    def __to_movie_budget_counter_pb(self, movie_budget_counter: MovieBudgetCounter):
+        movie_budget_counter_encoded = movie_budget_counters_pb2.MovieBudgetCounter()
+
+        movie_budget_counter_encoded.country = movie_budget_counter.country
+        movie_budget_counter_encoded.total_budget = movie_budget_counter.total_budget
+
+        return movie_budget_counter_encoded
+
+    def __encode_all(self, l):
+        return movie_budget_counters_pb2.MovieBudgetCounters(list=l).SerializeToString()
+
+    def __to_movie_budget_counter(self, movie_budget_counter_pb2) -> MovieBudgetCounter:
+        country = movie_budget_counter_pb2.country
+        total_budget = movie_budget_counter_pb2.total_budget
+
+        return MovieBudgetCounter(country, total_budget)
+
+    def __decode_all(self, buf: bytes, bytes_amount: int):
+        return movie_budget_counters_pb2.MovieBudgetCounters().ParseFromString(buf[0:bytes_amount]).list
