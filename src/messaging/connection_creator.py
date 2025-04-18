3538316a23cf5c29593f30e238f06a7a19c45181
SBen-IV <sinneo@fi.uba.ar>
2025-04-18 12:40:26 -0300

    feat: config and connection creator add input/output queue

--- src/messaging/connection_creator.py
+++ src/messaging/connection_creator.py
@@ -1,15 +1,23 @@
 from src.messaging.broker import RabbitMQBroker
 from src.messaging.connection import Connection
-from src.messaging.consumer import BroadcastConsumer
-from src.messaging.publisher import BroadcastPublisher
+from src.messaging.consumer import BroadcastConsumer, NamedQueueConsumer
+from src.messaging.publisher import BroadcastPublisher, DirectPublisher
 from src.utils.config import Config
 
 
 class ConnectionCreator():
-    @classmethod
-    def create(cls, config: Config) -> Connection:
+    @staticmethod
+    def create(config: Config) -> Connection:
         broker = RabbitMQBroker(config.rabbit_host)
-        publisher = BroadcastPublisher(broker, config.publisher_exchange)
-        consumer = BroadcastConsumer(broker, config.consumer_exchange)
+
+        if config.output_queue:
+            publisher = DirectPublisher(broker, config.output_queue)
+        else:
+            publisher = BroadcastPublisher(broker, config.publisher_exchange)
+
+        if config.input_queue:
+            consumer = NamedQueueConsumer(broker, config.input_queue)
+        else:
+            consumer = BroadcastConsumer(broker, config.consumer_exchange)
 
         return Connection(broker, publisher, consumer)
--- src/utils/config.py
+++ src/utils/config.py
@@ -11,6 +11,8 @@ class Config():
         self.rabbit_host = os.getenv("RABBIT_HOST", config["DEFAULT"]["RABBIT_HOST"])
         self.publisher_exchange = os.getenv("PUBLISHER_EXCHANGE", config["DEFAULT"]["PUBLISHER_EXCHANGE"])
         self.consumer_exchange = os.getenv("CONSUMER_EXCHANGE", config["DEFAULT"]["CONSUMER_EXCHANGE"])
+        self.output_queue = os.getenv("OUTPUT_QUEUE", config["DEFAULT"]["OUTPUT_QUEUE"])
+        self.input_queue = os.getenv("INPUT_QUEUE", config["DEFAULT"]["INPUT_QUEUE"])
 
     def get_env_var(self, var_name: str, default: str = None) -> str | None:
         return os.getenv(var_name, default)
