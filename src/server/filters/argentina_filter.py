ff5b27062e3f7d320f76e137c19a2fe557b6eca6
SBen-IV <sinneo@fi.uba.ar>
2025-04-22 14:58:41 -0300

    feat: argentina filter

---
+++ src/server/filters/argentina_filter.py
@@ -0,0 +1,26 @@
+import logging
+
+from src.model.movie import Movie
+
+from .base_filter_logic import BaseFilterLogic
+
+logger = logging.getLogger(__name__)
+
+
+class ArgentinaLogic(BaseFilterLogic):
+    def should_pass(self, movie: Movie) -> bool:
+        try:
+            if 'Argentina' in movie.production_countries:
+                return True
+            else:
+                return False
+        except Exception as e:
+            movie_id = getattr(movie, 'id', 'UNKNOWN')
+            logger.error(f'Error checking movie ID={movie_id} for argentina: {e}')
+            return False
+
+    def map(self, movie: Movie) -> Movie:
+        return Movie(
+            movie_id=movie.id,
+            title=movie.title,
+        )
