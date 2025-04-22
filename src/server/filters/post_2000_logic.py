3db72137fa14400cea84c8623c1e5006c09308ff
SBen-IV <sinneo@fi.uba.ar>
2025-04-22 14:54:29 -0300

    refactor: change log message

--- src/server/filters/post_2000_logic.py
+++ src/server/filters/post_2000_logic.py
@@ -16,7 +16,7 @@ class Post2000Logic(BaseFilterLogic):
                 return False
         except Exception as e:
             movie_id = getattr(movie, 'id', 'UNKNOWN')
-            logger.error(f'Error checking movie ID={movie_id} for solo country: {e}')
+            logger.error(f'Error checking movie ID={movie_id} for post 2000: {e}')
             return False
 
     def map(self, movie: Movie) -> Movie:
