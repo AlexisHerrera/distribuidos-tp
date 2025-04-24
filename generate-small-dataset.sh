e39d10ca3e6e98a029067822fa0cd0df751ef6b6
SBen-IV <sinneo@fi.uba.ar>
2025-04-24 19:00:15 -0300

    feat: specify size of dataset and move to .data-small

--- generate-small-dataset.sh
+++ generate-small-dataset.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+PERCENTAGE=${1:-10}
+
+DIR_SMALL=".data-small"
+
+mkdir -p ./${DIR_SMALL}
+
+echo "Truncating files by ${PERCENTAGE}%"
+
 MOVIES_FILE=movies_metadata.csv
 CREDITS_FILE=credits.csv
 RATINGS_FILE=ratings.csv
@@ -7,8 +15,8 @@ RATINGS_FILE=ratings.csv
 create_small_file() {
     local filename=$1
     local total_lines=$(wc -l < ${filename})
-    local small_file_lines=$((total_lines / 10))
-    head -n ${small_file_lines} ${filename} > small-${filename}
+    local small_file_lines=$((total_lines / PERCENTAGE))
+    head -n ${small_file_lines} ${filename} > ${DIR_SMALL}/${filename}
 }
 
 create_small_file ${MOVIES_FILE}
