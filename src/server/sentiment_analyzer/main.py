d7c59f156c2409a1b0e76df49a2002a3456c4c7f
Alexis Herrera <alexysaguilar@gmail.com>
2025-04-24 18:46:14 -0300

    Revert "refactor: sentiment analyzer on working thread"
    This reverts commit 537739dc342156d0d2942ead78a232ed58852390.

--- src/server/sentiment_analyzer/main.py
+++ src/server/sentiment_analyzer/main.py
@@ -1,5 +1,4 @@
 import logging
-from concurrent.futures import ThreadPoolExecutor
 
 from src.messaging.protocol.message import Message, MessageType
 from src.server.base_node import BaseNode
@@ -13,7 +12,6 @@ AVAILABLE_ANALYZER_LOGICS = {'sentiment': SentimentAnalyzer}
 class SentimentAnalyzerNode(BaseNode):
     def __init__(self, config: Config, analyzer_type: str):
         super().__init__(config, analyzer_type)
-        self.__executor = ThreadPoolExecutor(max_workers=1)
 
         logger.info(f"SentimentAnalyzerNode '{analyzer_type}' initialized.")
 
@@ -25,23 +23,11 @@ class SentimentAnalyzerNode(BaseNode):
             return
 
         if message.message_type == MessageType.Movie:
-            logic_result = []
-
-            movies = message.data
-            logic_result = self.__executor.map(self.logic.handle_message, [movies])
-
-            result = []
-            for r in logic_result:
-                result.extend(r)
-
+            result = self.logic.handle_message(message.data)
             out_message = Message(MessageType.MovieSentiment, result)
-
             self.connection.send(out_message)
         else:
             logger.warning(f'Unknown message type received: {message.message_type}')
-
-    def stop(self):
-        self.__executor.shutdown(cancel_futures=True)
 
 
 if __name__ == '__main__':
