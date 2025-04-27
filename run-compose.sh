876a02592102ff2898938856326edf74c7df4544
SBen-IV <sinneo@fi.uba.ar>
2025-04-27 17:17:06 -0300

    refactor: run-compose

--- run-compose.sh
+++ run-compose.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-CONFIG_FILE="${1:-run_config.txt}"
-FLAGS=()
+SMALL_DATASET=${1:-no}
+DATASET_PERCENT_SIZE=${2:-10}
+SMALL_DATASET_FLAG=
 
-while IFS='=' read -r key value; do
-  [[ -z "$key" || "$key" =~ ^# ]] && continue
-  if [[ "$value" =~ ^[0-9]+$ ]] && (( value > 0 )); then
-    FLAGS+=( "--${key}" "${value}" )
-  fi
-done < "$CONFIG_FILE"
+if [[ ${SMALL_DATASET} != "no" ]]; then
+  echo ">>> Generando small dataset"
+  SMALL_DATASET_FLAG="-s"
+  bash ./generate-small-dataset.sh ${DATASET_PERCENT_SIZE}
+fi
 
-echo ">>> Generando docker-compose-dev.yaml con:" "${FLAGS[@]}"
-python generate-compose.py "${FLAGS[@]}"
+echo ">>> Generando docker-compose-dev.yaml con ${SMALL_DATASET_FLAG}"
+python generate-compose.py ${SMALL_DATASET_FLAG}
 
 echo ">>> Lanzando contenedores"
-make docker-compose-up
+# make docker-compose-up
