ec5f9b052b7ee3592b98bdedcec031e3ed156af5
SBen-IV <sinneo@fi.uba.ar>
2025-05-06 16:36:25 -0300

    refactor: group by id q1

--- src/server/sinks/q1_arg_spa_2000_sink_logic.py
+++ src/server/sinks/q1_arg_spa_2000_sink_logic.py
@@ -10,18 +10,26 @@ logger = logging.getLogger(__name__)
 
 class Q1ArgSpa2000(BaseSinkLogic):
     def __init__(self):
-        self.final_movies_and_genres: list[Movie] = []
+        self.final_movies_and_genres: dict[int, list[Movie]] = {}
         logger.info('Q1ArgSpa2000 initialized.')
 
     def merge_results(self, message: Message):
         list_movies_genres: list[Movie] = message.data
+        user_id = message.user_id
         for movie_genre in list_movies_genres:
             # Asumimos id unico de movie, por lo que no necesito
             # agrupar por id, además que hice que el filtro lo saque
-            self.final_movies_and_genres.append(movie_genre)
+            partial_result = self.final_movies_and_genres.get(user_id, [])
+
+            partial_result.append(movie_genre)
+
+            self.final_movies_and_genres[user_id] = partial_result
 
     def message_result(self, user_id: int) -> Message:
         logger.info(f'Final Movie Genre: count {len(self.final_movies_and_genres)}')
-        for movies_genre in self.final_movies_and_genres:
+        for movies_genre in self.final_movies_and_genres[user_id]:
             logger.info(f'Movie: {movies_genre.title}, Genres: {movies_genre.genres}')
-        return Message(user_id, MessageType.Movie, self.final_movies_and_genres)
+
+        result = self.final_movies_and_genres.pop(user_id)
+
+        return Message(user_id, MessageType.Movie, result)
