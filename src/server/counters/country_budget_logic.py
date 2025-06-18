184f3532229f6cb7a96d20c497308bee9fe78bed
Alexis Herrera <alexysaguilar@gmail.com>
2025-06-17 23:04:58 -0300

    change counters to stateless

--- src/server/counters/country_budget_logic.py
+++ src/server/counters/country_budget_logic.py
@@ -1,5 +1,4 @@
 import logging
-import uuid
 
 from src.messaging.protocol.message import Message, MessageType
 from src.model.movie_budget_counter import MovieBudgetCounter
@@ -15,42 +14,25 @@ class CountryBudgetLogic(BaseCounterLogic):
         self.country_budgets = SafeDict()
         logger.info('CountryBudgetLogic initialized.')
 
-    def process_message(self, message: Message):
+    def process_message(self, message: Message) -> Message | None:
         movie_list = message.data
         user_id = message.user_id
-        partial_result: dict[str, int] = self.country_budgets.get(user_id, {})
+
+        batch_result: dict[str, int] = {}
 
         for movie in movie_list:
-            production_countries = movie.production_countries
-            country = production_countries[0]
-            budget = int(movie.budget)
-            partial_result[country] = partial_result.get(country, 0) + budget
+            if movie.production_countries:
+                country = movie.production_countries[0]
+                budget = int(movie.budget)
+                batch_result[country] = batch_result.get(country, 0) + budget
 
-        self.country_budgets.set(user_id, partial_result)
+        if not batch_result:
+            return None
 
-    def message_result(self, user_id: uuid.UUID) -> Message:
-        user_result = self.country_budgets.pop(user_id, {})
+        result_list = [MovieBudgetCounter(k, v) for k, v in batch_result.items()]
 
-        result = [MovieBudgetCounter(k, v) for k, v in user_result.items()]
-
-        logger.info(f'Se mando: {result}')
-
-        return Message(user_id, MessageType.MovieBudgetCounter, result)
-
-    def log_final_results(self, user_id: uuid.UUID):
-        result = self.country_budgets.get(user_id, {})
-        logger.info(f'--- Final Country Budget Counts for {user_id} ---')
-        if not result:
-            logger.info('No country budgets were counted.')
-            return
-
-        sorted_countries = sorted(
-            result.items(), key=lambda item: item[1], reverse=True
+        logger.info(
+            f'Processed batch for user {user_id}, sending {len(result_list)} partial budget counts.'
         )
 
-        logger.info('Top 5 Countries by Total Budget Invested (Single Production):')
-        for i, (country, total_budget) in enumerate(sorted_countries[:5]):
-            logger.info(f'  {i + 1}. {country}: {total_budget}')
-
-        logger.info(f'Total countries counted: {len(sorted_countries)}')
-        logger.info('-----------------------------')
+        return Message(user_id, MessageType.MovieBudgetCounter, result_list)
