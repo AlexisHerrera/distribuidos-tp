5cde9b7ea4d20053e056ec9015a40a206a43e60c
fedepenic <fedepenic@hotmail.com>
2025-06-23 20:51:28 +0000

    feat: create different config files for cast joiner.

--- create_multiqueues_config_files.py
+++ create_multiqueues_config_files.py
@@ -123,16 +123,62 @@ def create_config_files_ratings_joiner(clients_uuids: list[str], output_director
         print(f"Ratings joiner configuration file created: {joiner_config_filename}")
 
 
+def create_config_files_cast_joiner(clients_uuids: list[str], output_directory: str):
+    os.makedirs(output_directory, exist_ok=True)
+
+    for client_id in clients_uuids:
+        cast_config = {
+            'rabbit': {
+                'host': 'rabbitmq'
+            },
+            'connection': {
+                'consumer': [
+                    {
+                        'type': 'direct',
+                        'queue': f'credits_clean_{client_id}'
+                    }
+                ],
+                'publisher': [
+                    {
+                        'type': 'direct',
+                        'queue': 'movies_cast_joined'
+                    }
+                ],
+                'base_db': [
+                    {
+                        'type': 'broadcast',
+                        'queue': 'argentina_post_2000_cast',
+                        'exchange': 'argentina_post_2000'
+                    }
+                ]
+            },
+            'heartbeat': {
+                'port': 13434
+            },
+            'log': {
+                'level': 'INFO'
+            }
+        }
+
+        cast_config_filename = os.path.join(output_directory, f'cast_joiner_config_{client_id}.yaml')
+
+        with open(cast_config_filename, 'w') as file:
+            yaml.dump(cast_config, file, Dumper=IndentDumper, default_flow_style=False, sort_keys=False)
+
+        print(f"Cast joiner configuration file created: {cast_config_filename}")
+
+
 def main():
     uuid_csv_path = 'clients_uuids.csv'
     cleaner_config_path = 'src/server/cleaner/config.yaml'
-    joiner_config_dir = 'src/server/joiners/'
+    joiners_dir = 'src/server/joiners/'
 
     try:
         clients_uuids = read_uuids_from_csv(uuid_csv_path)
 
         create_config_file_cleaner(clients_uuids, cleaner_config_path)
-        create_config_files_ratings_joiner(clients_uuids, joiner_config_dir)
+        create_config_files_ratings_joiner(clients_uuids, joiners_dir)
+        create_config_files_cast_joiner(clients_uuids, joiners_dir)
 
     except Exception as e:
         print(f"Error: {e}")
